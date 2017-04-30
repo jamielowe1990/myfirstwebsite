65a8edd622e8c46120b722644fe21950b46645ac
jamielowe1990 <s-h-i-t_happens@hotmail.co.uk>
2017-04-30 15:08:02 +0100

    Create test file

--- web/index.php
+++ web/index.php
@@ -3,10 +3,9 @@
  * Created by PhpStorm.
  * User: Jamie Lowe
  * Date: 26/04/2017
- * Time: 20:35
+ * Time: 22:15
  */
 
-
-echo "Hello world!";
+echo "<h1>Testing my first webpage upload!</h1>";
 
 ?>
