212cc9eb798e7d4e14035f67d002127a1821bc0f
jamielowe1990 <s-h-i-t_happens@hotmail.co.uk>
2017-04-30 20:59:07 +0100

    Tweaked template, now a half decent home page,

--- web/index.php
+++ web/index.php
@@ -1,19 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jamie Lowe
- * Date: 26/04/2017
- * Time: 22:15
+/*
+ * A side project website to showcase all of the stuff that I get up to...
+ * Travel, Photography and PHP coding.
+ *
  */
+class MyWebsite {
 
-/*echo "<h1>Testing my first webpage upload!</h1>";*/
-
-$page = <<<HTML
+  public function run() {
+    $page = <<<HTML
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <!-- Theme Made By www.w3schools.com - No Copyright -->
-  <title>Bootstrap Theme The Band</title>
+  <title>Jamie Lowe</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -37,9 +35,9 @@ $page = <<<HTML
   }
   .person {
       border: 10px solid transparent;
-      margin-bottom: 25px;
-      width: 80%;
-      height: 80%;
+      margin-bottom: 15px;
+      width: 35%;
+      height: 35%;
       opacity: 0.7;
   }
   .person:hover {
@@ -109,8 +107,6 @@ $page = <<<HTML
   #googleMap {
       width: 100%;
       height: 400px;
-      -webkit-filter: grayscale(100%);
-      filter: grayscale(100%);
   }
   .navbar {
       font-family: Montserrat, sans-serif;
@@ -178,150 +174,81 @@ $page = <<<HTML
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#myPage">HOME</a></li>
-        <li><a href="#band">BAND</a></li>
-        <li><a href="#tour">TOUR</a></li>
+        <li><a href="#about">ABOUT ME</a></li>
+        <li><a href="#photography">PHOTOGRAPHY</a></li>
+        <li><a href="#travel">TRAVEL</a></li>
+        <li><a href="#work">WORK & PROJECTS</a></li>
         <li><a href="#contact">CONTACT</a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">MORE
-          <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Merchandise</a></li>
-            <li><a href="#">Extras</a></li>
-            <li><a href="#">Media</a></li>
-          </ul>
-        </li>
         <li><a href="#"><span class="glyphicon glyphicon-search"></span></a></li>
       </ul>
     </div>
   </div>
 </nav>
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img src="ny.jpg" alt="New York" width="1200" height="700">
-        <div class="carousel-caption">
-          <h3>New York</h3>
-          <p>The atmosphere in New York is lorem ipsum.</p>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="chicago.jpg" alt="Chicago" width="1200" height="700">
-        <div class="carousel-caption">
-          <h3>Chicago</h3>
-          <p>Thank you, Chicago - A night we won't forget.</p>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="la.jpg" alt="Los Angeles" width="1200" height="700">
-        <div class="carousel-caption">
-          <h3>LA</h3>
-          <p>Even though the traffic was a mess, we had the best time playing at Venice Beach!</p>
-        </div>
-      </div>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-</div>
-
-<!-- Container (The Band Section) -->
-<div id="band" class="container text-center">
-  <h3>THE BAND</h3>
-  <p><em>We love music!</em></p>
-  <p>We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+<!-- Container - About Me -->
+<div id="about" class="container text-center">
+  <h1>ABOUT ME</h1>
   <br>
   <div class="row">
-    <div class="col-sm-4">
-      <p class="text-center"><strong>Name</strong></p><br>
-      <a href="#demo" data-toggle="collapse">
-        <img src="me.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
-      </a>
-      <div id="demo" class="collapse">
-        <p>Guitarist and Lead Vocalist</p>
-        <p>Loves long walks on the beach</p>
-        <p>Member since 1988</p>
-      </div>
+    <div class="col-md-4">
+       <h2><p class="lead">Travel. Photography. PHP.</p></h2>
+       <h3><p><em>Welcome to my world.</em></p></h3>
     </div>
-    <div class="col-sm-4">
-      <p class="text-center"><strong>Name</strong></p><br>
-      <a href="#demo2" data-toggle="collapse">
-        <img src="bandmember.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
-      </a>
-      <div id="demo2" class="collapse">
-        <p>Drummer</p>
-        <p>Loves drummin'</p>
-        <p>Member since 1988</p>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <p class="text-center"><strong>Name</strong></p><br>
-      <a href="#demo3" data-toggle="collapse">
-        <img src="bandmember.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
-      </a>
-      <div id="demo3" class="collapse">
-        <p>Bass player</p>
-        <p>Loves math</p>
-        <p>Member since 2005</p>
-      </div>
+    <div class="col-md-8">
+       <img src="me.jpg" class="rounded mx-auto d-block person" alt="Oops! Image Not Available" width="50" height="50">
     </div>
   </div>
 </div>
 
-<!-- Container (TOUR Section) -->
-<div id="tour" class="bg-1">
+<!-- Container - Photography -->
+<div id="photography" class="bg-1">
   <div class="container">
-    <h3 class="text-center">TOUR DATES</h3>
-    <p class="text-center">Lorem ipsum we'll play you some music.<br> Remember to book your tickets!</p>
-    <ul class="list-group">
-      <li class="list-group-item">September <span class="label label-danger">Sold Out!</span></li>
-      <li class="list-group-item">October <span class="label label-danger">Sold Out!</span></li>
-      <li class="list-group-item">November <span class="badge">3</span></li>
-    </ul>
+    <h3 class="text-center">PHOTOGRAPHY</h3>
+    <p class="text-center">From time to time, I attempt to take a decent photo or two... Here's some of my favourite shots from recent outings</p>
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
-    <div class="row text-center">
-      <div class="col-sm-4">
-        <div class="thumbnail">
-          <img src="paris.jpg" alt="Paris" width="400" height="300">
-          <p><strong>Paris</strong></p>
-          <p>Friday 27 November 2015</p>
-          <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="ny.jpg" alt="New York" width="1200" height="700">
+            <div class="carousel-caption">
+              <h3>New York</h3>
+              <p>The atmosphere in New York is lorem ipsum.</p>
+            </div>
+          </div>
+
+          <div class="item">
+            <img src="chicago.jpg" alt="Chicago" width="1200" height="700">
+            <div class="carousel-caption">
+              <h3>Chicago</h3>
+              <p>Thank you, Chicago - A night we won't forget.</p>
+            </div>
+          </div>
+
+          <div class="item">
+            <img src="la.jpg" alt="Los Angeles" width="1200" height="700">
+            <div class="carousel-caption">
+              <h3>LA</h3>
+              <p>Even though the traffic was a mess, we had the best time playing at Venice Beach!</p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="thumbnail">
-          <img src="newyork.jpg" alt="New York" width="400" height="300">
-          <p><strong>New York</strong></p>
-          <p>Saturday 28 November 2015</p>
-          <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
-        </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="thumbnail">
-          <img src="sanfran.jpg" alt="San Francisco" width="400" height="300">
-          <p><strong>San Francisco</strong></p>
-          <p>Sunday 29 November 2015</p>
-          <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
-        </div>
-      </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
     </div>
   </div>
 
@@ -354,24 +281,81 @@ $page = <<<HTML
           <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal">
             <span class="glyphicon glyphicon-remove"></span> Cancel
           </button>
-          <p>Need <a href="#">help?</a></p>
+          <p>Need <a href="#contact">help?</a></p>
         </div>
       </div>
     </div>
   </div>
 </div>
 
+<div id="travel" class="container">
+    <h3 class="text-center">TRAVEL</h3>
+    <p class="text-center">My first real travelling experience was back in 2012 when I visited Australia to fulfil my dream. I haven't stopped exploring the world since.</p>
+    <div class="row text-center">
+        <ul class="nav nav-tabs">
+          <li class="active"><a data-toggle="tab" href="#home">Up Next...</a></li>
+          <li><a data-toggle="tab" href="#menu1">The Philippines</a></li>
+          <li><a data-toggle="tab" href="#menu2">Bali</a></li>
+        </ul>
+
+        <div class="tab-content">
+          <div id="home" class="tab-pane fade in active">
+            <h2>Budapest, Hungary.</h2>
+            <p>???</p>
+          </div>
+          <div id="menu1" class="tab-pane fade">
+            <h2>March 2017</h2>
+            <p>???</p>
+          </div>
+          <div id="menu2" class="tab-pane fade">
+            <h2>June 2016</h2>
+            <p>???</p>
+          </div>
+        </div>
+    </div>
+</div>
+
+<div id="work" class="bg-1">
+    <div class="container">
+      <h3 class="text-center">WORK & PROJECTS</h3>
+      <p class="text-center">My current role as a CRM Solutions Consultant keeps me busy, busy, busy. However, in my spare time I like to build on my skills and try my hand at PHP.</p>
+      <div class="row text-center">
+          <div class="col-sm-4">
+            <div class="thumbnail">
+              <img src="sm.png" alt="Survey Monkey" width="100" height="100">
+              <p><strong>Survey Monkey</strong></p>
+              <p>2017</p>
+              <button class="btn">See More</button>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="thumbnail">
+              <img src="gtw.jpg" alt="GoTo Webinar" width="100" height="100">
+              <p><strong>GoTo Webinar</strong></p>
+              <p>2017</p>
+              <button class="btn">See More</button>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="thumbnail">
+              <img src="question.png" alt="Coming Soon..." width="100" height="100">
+              <p><strong>???</strong></p>
+              <p>2017</p>
+              <button class="btn">See More</button>
+            </div>
+          </div>
+      </div>
+    </div>
+</div>
+
 <!-- Container (Contact Section) -->
 <div id="contact" class="container">
   <h3 class="text-center">Contact</h3>
-  <p class="text-center"><em>We love our fans!</em></p>
-
   <div class="row">
     <div class="col-md-4">
-      <p>Fan? Drop a note.</p>
-      <p><span class="glyphicon glyphicon-map-marker"></span>Chicago, US</p>
-      <p><span class="glyphicon glyphicon-phone"></span>Phone: +00 1515151515</p>
-      <p><span class="glyphicon glyphicon-envelope"></span>Email: mail@mail.com</p>
+      <p>Would you like to know more?</p>
+      <p>Get in touch.</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span>Reading, United Kingdom</p>
     </div>
     <div class="col-md-8">
       <div class="row">
@@ -391,53 +375,26 @@ $page = <<<HTML
       </div>
     </div>
   </div>
-  <br>
-  <h3 class="text-center">From The Blog</h3>
-  <ul class="nav nav-tabs">
-    <li class="active"><a data-toggle="tab" href="#home">Mike</a></li>
-    <li><a data-toggle="tab" href="#menu1">Chandler</a></li>
-    <li><a data-toggle="tab" href="#menu2">Peter</a></li>
-  </ul>
-
-  <div class="tab-content">
-    <div id="home" class="tab-pane fade in active">
-      <h2>Mike Ross, Manager</h2>
-      <p>Man, we've been on the road for some time now. Looking forward to lorem ipsum.</p>
-    </div>
-    <div id="menu1" class="tab-pane fade">
-      <h2>Chandler Bing, Guitarist</h2>
-      <p>Always a pleasure people! Hope you enjoyed it as much as I did. Could I BE.. any more pleased?</p>
-    </div>
-    <div id="menu2" class="tab-pane fade">
-      <h2>Peter Griffin, Bass player</h2>
-      <p>I mean, sometimes I enjoy the show, but other times I enjoy other things.</p>
-    </div>
-  </div>
 </div>
 
 <!-- Add Google Maps -->
 <div id="googleMap"></div>
 <script>
-function myMap() {
-var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-var marker = new google.maps.Marker({position:myCenter});
-marker.setMap(map);
-}
+  function myMap() {
+  var myCenter = new google.maps.LatLng(51.451457,-0.996048);
+  var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+  }
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
-<!--
-To use this code on your website, get a free API key from Google.
-Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
--->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7XkZ3567WIF2pFd6GDVFTKlQBv7zShzY&callback=myMap"></script>
 
 <!-- Footer -->
 <footer class="text-center">
-  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="Home">
     <span class="glyphicon glyphicon-chevron-up"></span>
-  </a><br><br>
-  <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" data-toggle="tooltip" title="Visit w3schools">www.w3schools.com</a></p>
+  </a>
 </footer>
 
 <script>
@@ -450,9 +407,7 @@ $(document).ready(function(){
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-
-      // Prevent default anchor click behavior
-      event.preventDefault();
+      event.preventDefault();  // Prevent default anchor click behavior
 
       // Store hash
       var hash = this.hash;
@@ -470,12 +425,12 @@ $(document).ready(function(){
   });
 })
 </script>
-
 </body>
 </html>
-
 HTML;
-
-echo $page;
-
+    return $page;
+  }
+}
+$website = new MyWebsite();
+echo $website->run();
 ?>
