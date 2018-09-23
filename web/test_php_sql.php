784b7dd379d7e48f0d4d637278a0e041ab0c1938
jamielowe1990 <s-h-i-t_happens@hotmail.co.uk>
2018-09-23 22:41:31 +0100

    Setup of Mysql & PHP to a localhost on MBP

---
+++ web/test_php_sql.php
@@ -0,0 +1,7 @@
+<?php
+$con = new mysqli("127.0.0.1", "root", "d9ehZKprvmCy", "mydb");
+$message = $con->query("SELECT message FROM myTable")->fetch_object()->message;
+$con->close();
+echo "$message <br/>";
+echo "Hello From Sites Folder!";
+phpinfo();
