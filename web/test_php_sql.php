02b9920db9e3367fcddee388a5ddb21eab78b8b1
jamielowe1990 <s-h-i-t_happens@hotmail.co.uk>
2018-09-29 21:43:03 +0100

    Remove test files from Git.

--- web/test_php_sql.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = new mysqli("127.0.0.1", "root", "d9ehZKprvmCy", "mydb");
-$message = $con->query("SELECT message FROM myTable")->fetch_object()->message;
-$con->close();
-echo "$message <br/>";
-echo "Hello From Sites Folder!";
-
-echo "I made some changes - does this commit to Git?";
-phpinfo();
